15fe0d8d4e5e0c2ae7c3dba0a84ae7954859bd71
seeo <aragonlotr5@gmail.com>
2019-05-05 19:31:24 +0800

    included validation stuff into park.rb model. with validates: name and validate: has_one_ranger_at_least

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,7 +1,7 @@
 class Park < ActiveRecord::Base
   has_many :ranger
   validates :name, presence: true, length: { minimum: 3, maximum: 20 }
-
+  validate :has_one_ranger_at_least
   def self.banana
 
   end
